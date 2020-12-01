7d09aa66fa9a79f9ab35efec41964a63eb5a6725
Rahul Singh <rkant225@gmail.com>
2020-12-02 01:04:24 +0530

    Done changes to modify APP Environment via environmental variable.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import store from './Redux/Store/store';
 import App from './App';
 
-console.log('ENV -> ', process.env.NODE_ENV === 'production' ? 'PRODUCTION' : 'DEVELOPMENT');
+console.log('ENV -> ', process.env.REACT_APP_ENV === 'production' ? 'PRODUCTION' : 'DEVELOPMENT');
 
 if (process.env.NODE_ENV === 'production') {
     console.log = ()=>{};
